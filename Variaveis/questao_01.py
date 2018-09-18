063ac111889231364b528a703055dc04fdb401d2
Ranyelson Dantas <nielsonnp@gmail.com>
2018-09-18 13:01:02 -0300

    Soma de dois números

---
+++ Variaveis/questao_01.py
@@ -0,0 +1,9 @@
+
+# 1. Faça um programa que peça dois números e imprima a soma.
+
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+
+soma = (num1 + num2)
+
+print('A soma de {} e {} é = {} ' .format(num1, num2, soma))
