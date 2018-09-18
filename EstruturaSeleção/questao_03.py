2acc5c5d4455ee2bac67406b6d506189a2358721
Ranyelson Dantas <nielsonnp@gmail.com>
2018-09-18 14:45:38 -0300

    Verificar se a Letra digitada é F ou M

---
+++ EstruturaSeleção/questao_03.py
@@ -0,0 +1,12 @@
+
+''' 3. Faça um Programa que verifique se uma letra digitada é
+"F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino.'''
+
+letra = input('Digite F - Feminino , M - Masculino: ')
+
+if letra == 'F':
+    print('A letra digita é F - Feminino')
+elif letra == 'M':
+    print('A letra digitada é M - Masculino')
+else:
+    print('Letra inválida')
