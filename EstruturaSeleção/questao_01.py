49d2c1f3edbd5b057544421a318516ba5b61900e
Ranyelson Dantas <nielsonnp@gmail.com>
2018-09-18 14:00:31 -0300

    Número Positivo ou Negativo

--- EstruturaSeleção/questao_01.py
+++ EstruturaSeleção/questao_01.py
@@ -3,7 +3,7 @@
 
 numero = int(input('Digite um número: '))
 
-if (numero % 2) == 0:
-    print('O número {} é par '.format(numero))
+if numero >= 0:
+    print('O número {} é positivo'.format(numero))
 else:
-    print('O número {} é impar '.format(numero))
+    print('O número {} é Negativo '.format(numero))
