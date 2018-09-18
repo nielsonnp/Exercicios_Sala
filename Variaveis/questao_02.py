bca23d5a7fe93872b023bbad5d691f9af10f531e
Ranyelson Dantas <nielsonnp@gmail.com>
2018-09-18 13:06:30 -0300

    Área de um circulo

---
+++ Variaveis/questao_02.py
@@ -0,0 +1,8 @@
+# 2. Faça um programa que peça o raio de um círculo, calcule e mostre sua área (A=π*r2).
+
+raio = float(input("Digite o raio do circulo: "))
+pi = 3.14
+
+area = (pi*raio**2)
+
+print('Área do circulo : {} ' .format(area))
