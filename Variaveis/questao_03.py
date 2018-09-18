6d428a513c4c89e0c7a49a54cfdec162a7cdde1a
Ranyelson Dantas <nielsonnp@gmail.com>
2018-09-18 13:08:53 -0300

    Converter m/cm

---
+++ Variaveis/questao_03.py
@@ -0,0 +1,7 @@
+# 3. Faça um programa que converta metros para centímetros (1m=100cm).
+
+metros = float(input('Digite a quantiade de metros: '))
+
+resultado = (metros * 100)
+
+print('{} m é = {:.2f} cm' .format(metros, resultado))
