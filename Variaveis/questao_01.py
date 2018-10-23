4fc08f1f60aeea2ac9dee5c70f66b55d41623168
Ranyelson Dantas <nielsonnp@gmail.com>
2018-10-23 11:21:44 -0300

    Soma de dois números

--- Variaveis/questao_01.py
+++ Variaveis/questao_01.py
@@ -6,4 +6,4 @@ num2 = int(input('Digite outro número: '))
 
 soma = (num1 + num2)
 
-print('A soma de {} e {} é = {} ' .format(num1, num2, soma))
+print('A soma de {} + {} = {} ' .format(num1, num2, soma))
