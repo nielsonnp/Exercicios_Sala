afab7cc1e8f66cbbb73c0a13ed9e9037071c67ee
Ranyelson Dantas <nielsonnp@gmail.com>
2018-10-24 13:08:13 -0300

    For - Programa que ler 5 números e informa o maior.

--- Repetições/questao_05.py
+++ Repetições/questao_05.py
@@ -1,11 +1,10 @@
-'''5. Faça um programa que leia 5 números e informe o maior e menor número.'''
+'''5. Faça um programa que leia 5 números e informe o maior.'''
 
-maior = 0
-menor = 1000
-for i in range (1,6):
+num = int(input('Digite um número: '))
+maior = num
+
+for i in range (1,5):
     n = int(input('Digite um número: '))
     if n > maior:
         maior = n
-    if n < menor:
-        menor = n
-print('O menor número é {} e o maior número é {}'.format(menor, maior))
+print('O maior número é {}'.format(maior))
