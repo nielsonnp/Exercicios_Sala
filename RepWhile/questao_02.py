2cae6873f1de15fcfb08fb07cdbaed7581bd3c3f
Ranyelson Dantas <nielsonnp@gmail.com>
2018-10-30 14:57:35 -0300

    While - Imprime na tela apenas os números ímpares entre 1 e 50, inclusive.

---
+++ RepWhile/questao_02.py
@@ -0,0 +1,10 @@
+'''2. Faça um programa que, utilizando o laço while, imprima na tela apenas os números
+ímpares entre 1 e 50, inclusive'''
+
+x = 1
+
+while x <= 50:
+    #if x % 2 == 1: #com IF, x = x + 1
+    print(x)
+    x = x + 2
+
