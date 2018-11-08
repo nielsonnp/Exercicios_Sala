effa0b1ec744c1433e04174876ab8fc20d621645
Ranyelson Dantas <nielsonnp@gmail.com>
2018-11-08 14:40:24 -0300

    While - Pede 2 números várias vezes para calcular potência. Com while e função. Digitando zero ou negativo para.

--- RepWhile/questao_06.py
+++ RepWhile/questao_06.py
@@ -2,18 +2,17 @@
 primeiro número elevado ao segundo número. Escreva uma função para calcular a potência.
 pare quando digitar números negativos ou zero'''
 
-def potencia ():
-    x = 1
-    while True:
-        n1 = int(input('Digite a base: '))
-        n2 = int(input('Digite o expoente: '))
-        p = n1**n2
-        print(p)
-        x = x + 1
-        if n1 <= 0 or n2 <= 0:
-            print('Erro: Número digitado menor ou igual a zero')
-            break
-potencia()
 
+def potencia (num1,num2):
+    p = num1 ** num2
+    print(p)
 
-
+x = 1
+while True:
+    n1 = int(input('Digite a base: '))
+    n2 = int(input('Digite o expoente: '))
+    if n1 <= 0 or n2 <= 0:
+        print('Erro: Número digitado menor ou igual a zero')
+        break
+    x = x + 1
+    potencia(n1,n2)
